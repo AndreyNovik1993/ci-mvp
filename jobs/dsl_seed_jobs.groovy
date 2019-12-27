65b0ad772f7a3cdcb6400220aa2b340ebe626ab5
Andrei Novik <novikandre93@gmail.com>
2019-12-27 09:36:07 +0300

    create a seed job and config.xml

---
+++ jobs/dsl_seed_jobs.groovy
@@ -0,0 +1,21 @@
+folder('job_dsl_seed') { 
+
+   description('folder for pipeline job')
+
+}
+pipelineJob('job_dsl_seed/pipeline') {
+      def repo = 'https://github.com/AndreyNovik1993/ci-mvp.git' 
+      description("Pipeline for $repo") 
+      definition { 
+           cpsScm { 
+               scm { 
+                 git { remote { url(repo) } 
+                   	branches('master') 
+               	   	scriptPath('Jenkinsfile_curl') 
+                    	extensions { } 
+		    }
+	  } 
+      } 
+   } 
+queue('job_dsl_seed/pipeline')
+}
