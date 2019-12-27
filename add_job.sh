843a8c8a1a7dc679df002621bbfa1cd8ca5bb160
Andrei Novik <novikandre93@gmail.com>
2019-12-27 13:45:50 +0300

    change a script and change jenkins security polices

--- add_job.sh
+++ add_job.sh
@@ -2,11 +2,11 @@
 curl -s -XPOST 'http://localhost:8080/createItem?name=seed_jobs_test' -u admin:admin  --data-binary @config.xml -H "Content-Type:text/xml" > /dev/null 
 if [ $? -eq 0 ] ;  
   then echo 'job add in jenkins and create pipeline. Please wait 60s when all enviroment start'
-  sleep 60 && curl -X POST http://admin:admin@localhost:8080/job_dsl/seed/build
+  sleep 60 && curl -X POST http://admin:admin@localhost:8080/job/seed_jobs_test/build
   if [ $? -eq 0 ]
     then echo 'job seeb will work  after 4s' 
   else
-    curl -X POST http://admin:admin@localhost:8080/job/seed/build
+    curl -X POST http://admin:admin@localhost:8080/job/seed_jobs_test/build
   fi
 else 
    echo 'job do not add jenkins, starting jenkins conteiner' 
